87bb6fbff5833dc064a18f2178530cd1285b611c
dinkol <102145254+dinkol@users.noreply.github.com>
2025-02-04 10:54:26 +0100

    Update storage.tf

--- storage.tf
+++ storage.tf
@@ -4,16 +4,4 @@ resource "azurerm_storage_account" "example" {
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  tags = {
-    environment = "testing"
-  }
-}
-
-resource "azurerm_storage_container" "vm_files" {
-  name = "tf-directory"
-  #S storage_account_id    = azurerm_storage_account.example.id
-  container_access_type = "private"
-  #storage_account_name  = azurerm_storage_account.example.name
-  storage_account_id    = azurerm_storage_account.example.id
-
 }
