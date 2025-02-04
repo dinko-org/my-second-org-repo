89022eed584b02aa8858c686ec6d4663a811ec15
dinkol <102145254+dinkol@users.noreply.github.com>
2025-02-04 09:22:03 +0100

    Update storage.tf

--- storage.tf
+++ storage.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "example" {
-  name                     = "dinkostoragevm"
+  name                     = "dinkotfstorage"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
@@ -15,20 +15,5 @@ resource "azurerm_storage_container" "vm_files" {
   container_access_type = "private"
   #storage_account_name  = azurerm_storage_account.example.name
   storage_account_id    = azurerm_storage_account.example.id
-
-}
-
-resource "azurerm_private_endpoint" "storage_private_endpoint" {
-  name                = "storage-private-endpoint"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  subnet_id           = azurerm_subnet.private_endpoints_subnet.id
-
-  private_service_connection {
-    name                           = "storage-private-service-connection"
-    private_connection_resource_id = azurerm_storage_account.example.id
-    subresource_names              = ["blob"]
-    is_manual_connection           = false
-  }
 
 }
