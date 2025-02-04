b17f2b23656a1a9e227b1e49bf1bc1b4c68f12cb
dinkol <102145254+dinkol@users.noreply.github.com>
2025-02-04 12:30:20 +0100

    Update sa name

--- storage.tf
+++ storage.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "example" {
-  name                     = "dinkotfstorage"
+  name                     = "dinkoltfstorage"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
