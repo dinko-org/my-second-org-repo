71a5b6a74e3fe58ddfd11a6710f814eb7dba68ed
dinkol <102145254+dinkol@users.noreply.github.com>
2025-02-10 15:29:24 +0100

    Update storage.tf

--- storage.tf
+++ storage.tf
@@ -1,15 +1,22 @@
 resource "azurerm_storage_account" "example" {
-  name                     = "dinkoltfstorage"
+  name                     = "sa${random_string.storage_suffix.result}"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
-
+/*
 resource "azurerm_storage_account" "newsa" {
   name                     = "oknidlsa"
   resource_group_name      = var.rg_name
   location                 = var.rg_location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+}
+*/
+resource "random_string" "storage_suffix" {
+  length  = 3
+  upper   = false
+  special = false
+  number  = true
 }
