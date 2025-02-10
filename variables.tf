cd557715b0dc7c7747ff55d92fd2257db85e0c2d
dinkol <102145254+dinkol@users.noreply.github.com>
2025-02-10 10:34:36 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
-variable "name" {
+variable "rg_name" {
   description = "The name of the resource group in which the resources will be created."
   type = string
 }
 
-variable "location" {
+variable "rg_location" {
   description = "The location of the resource group in which the resources will be created."
   type = string
 }
