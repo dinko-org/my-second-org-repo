09a8c6b51d0b47110f04a8fd46209ff2ff06c1ef
dinkol <102145254+dinkol@users.noreply.github.com>
2025-02-10 14:53:37 +0100

    added rg's as environments

--- resourcegroups.tf
+++ resourcegroups.tf
@@ -1,5 +1,15 @@
-resource "azurerm_resource_group" "example" {
-  name     = "dinkol-tf-rg"
+resource "azurerm_resource_group" "prod" {
+  name     = "dinkol-tf-rg-prod"
+  location = "eastus"
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "dinkol-tf-rg-test"
+  location = "eastus"
+}
+
+resource "azurerm_resource_group" "dev" {
+  name     = "dinkol-tf-rg-dev"
   location = "eastus"
 }
 
@@ -7,3 +17,4 @@ resource "azurerm_resource_group" "rg" {
   name = var.rg_name
   location = var.rg_location
 }
+
