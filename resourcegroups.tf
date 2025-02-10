eac9cc19146d5ee3ed539e809983e2023a3e5531
dinkol <102145254+dinkol@users.noreply.github.com>
2025-02-10 10:34:18 +0100

    Update resourcegroups.tf

--- resourcegroups.tf
+++ resourcegroups.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "example" {
   name     = "dinkol-tf-rg"
   location = "eastus"
 }
+
+resource "azurerm_resource_group" "rg" {
+  name = var.rg_name
+  location = var.rg_location
+}
